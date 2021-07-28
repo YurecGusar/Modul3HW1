3f47353d151d5fb9f173ebf93c0d895a6f676c6c
Yurii <leoonovn@gmail.com>
2021-07-28 13:52:09 +0300

    Пустой проект

---
+++ Modul3HW1/Modul3HW1/Program.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Modul3HW1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var serviceProvider = new ServiceCollection()
+                .AddTransient<Starter>()
+                .BuildServiceProvider();
+
+            var starter = serviceProvider.GetService<Starter>();
+            starter.Run();
+        }
+    }
+}
